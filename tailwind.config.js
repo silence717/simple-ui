196dc1f1f9a18a77af6a476f63f28b7b48e483cc
fang.yang <yangfang2028@126.com>
2021-09-08 23:30:49 +0800

    ➕ Add tailwindcss dependency

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const colors = require('./tailwind-color');
+
+module.exports = {
+  mode: 'jit',
+  purge: [],
+  darkMode: false,
+  theme: {
+    colors: colors,
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
